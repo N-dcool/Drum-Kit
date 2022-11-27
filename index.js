2e747b81f40168c8deaba284465160de90813587
Cnaresh02 <nareshchoudhary@NARESHs-MacBook-Pro.local>
2022-11-27 14:43:10 +0530

    updated html and js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfButtons; i++){
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var audio = new Audio('/sounds/tom-1.mp3');
+        var audio = new Audio('sounds/tom-1.mp3');
         audio.play();    
         makeSound(this.innerHTML)
 
