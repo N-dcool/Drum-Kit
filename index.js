84c352a33e9ab8ca7d39341d722aa889b831c8a3
Cnaresh02 <nareshchoudhary@NARESHs-MacBook-Pro.local>
2022-11-27 13:49:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfButtons; i++){
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var audio = new Audio('/sounds/tom-1.mp3');
+        audio.play();    
+        makeSound(this.innerHTML)
+
+        });
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    buttonAnimation(key);
+    switch(key){
+        case "w":
+            var audio = new Audio('/sounds/crash.mp3');
+            audio.play();
+        break;
+
+        case "a":
+            var audio = new Audio('/sounds/kick-bass.mp3');
+            audio.play();
+        break;
+
+        case "s":
+            var audio = new Audio('/sounds/snare.mp3');
+            audio.play();
+        break;
+
+        case "d":
+            var audio = new Audio('/sounds/tom-1.mp3');
+            audio.play();
+        break;
+
+        case "j":
+            var audio = new Audio('/sounds/tom-2.mp3');
+            audio.play();
+        break;
+
+        case "k":
+            var audio = new Audio('/sounds/tom-3.mp3');
+            audio.play();
+        break;
+
+        case "l":
+            var audio = new Audio('/sounds/tom-4.mp3');
+            audio.play();
+        break;            
+    }
+}
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+
+
+
+
+
+
+
